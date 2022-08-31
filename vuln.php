a1a02ed7bf09346f2e1d6916663d7cc0387b2a79
Christophe H <65390576+christophe-havard-sonarsource@users.noreply.github.com>
2022-08-31 16:16:45 +0200

    Update vuln.php

--- vuln.php
+++ vuln.php
@@ -1,3 +1,42 @@
-<?php
+<?php 
 
-  machin();
+use Symfony\Component\HttpFoundation\Request;
+
+class Controller
+{
+    /**
+     * @var \Doctrine\DBAL\Connection
+     */
+    protected $connection;
+
+    public function sqlQuery1(Request $request)
+    {
+        $userId = $request->get('id');
+        $sql = "SELECT email FROM user WHERE id='$userId'";
+        $statement = $this->connection->prepare($sql);
+        $statement->execute();
+        $username = $statement->fetchColumn();
+        return $this->json(['email' => $username]);
+    }
+    public function newVulnFunction(Request $request)
+    {
+        $userId = $request->get('id');
+        $sql = "SELECT username FROM user WHERE id='$userId'";
+        $statement = $this->connection->prepare($sql);
+        $statement->execute();
+        $username = $statement->fetchColumn();
+        return $this->json(['email' => $username]);
+    }
+    public function newNewVulnFunction(Request $request)
+    {
+        $userId = $request->get('id');
+        $sql = "SELECT username FROM user WHERE id='$userId'";
+        $statement = $this->connection->prepare($sql);
+        $statement->execute();
+        $username = $statement->fetchColumn();
+        return $this->json(['email' => $username]);
+    }
+    
+}
+
+?>
